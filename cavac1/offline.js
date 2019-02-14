96c56f0901758f0e67cf894221957a0594205ca1
gabrieldoviolao <gabrielmelo@id.uff.br>
2019-02-14 01:01:39 -0200

    Add files via upload

---
+++ cavac1/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1550112944,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bracocavaquinho-sheet0.png",
+		"images/spritecordad-sheet0.png",
+		"images/spritecordag-sheet0.png",
+		"images/spritecordab-sheet0.png",
+		"images/spritecordadaguda-sheet0.png",
+		"media/b.ogg",
+		"media/d.ogg",
+		"media/daguda.ogg",
+		"media/g.ogg",
+		"media/impact.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
